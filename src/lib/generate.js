0f8f3d2e97f8335af5418630d842a13ebcc9bd47
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-05-14 17:36:55 +0100

    Fix generateKey values

--- src/lib/generate.js
+++ src/lib/generate.js
@@ -11,10 +11,10 @@ const generateKey = async(data) => {
   const { privateKey, publicKey } = await openpgp.generateKey({
     curve: String(data.curve), // Elliptic curve for ECC keys: curve25519 (default), p256, p384, p521, secp256k1, brainpoolP256r1, brainpoolP384r1, or brainpoolP512r1
     format: data.format, // format of the output keys e.g. 'armored' | 'object' | 'binary';
-    keyExpirationTime: 0, // Number of seconds from the key creation time after which the key expires
+    keyExpirationTime: parseInt(data.expiration), // Number of seconds from the key creation time after which the key expires
     passphrase: data.passphrase, // The passphrase used to encrypt the generated private key. If omitted, the key won't be encrypted. e.g. 1234567890abcdef
     rsaBits: parseInt(data.size), // Number of bits for RSA keys (defaults to 4096 bits)
-    subkeys: [{sign: true, passphrase: data.passphrase}], // Options for each subkey e.g. [{sign: true, passphrase: '123'}] default to main key options, except for sign parameter that defaults to false, and indicates whether the subkey should sign rather than encrypt
+    subkeys: [{sign: data.sign, passphrase: data.passphrase}], // Options for each subkey e.g. [{sign: true, passphrase: '123'}] default to main key options, except for sign parameter that defaults to false, and indicates whether the subkey should sign rather than encrypt
     type: String(data.type), // The primary key algorithm type: ECC (default) or RSA
     userIDs: [{ name: data.name, email: data.email }] // User IDs as objects: [{ name: "John Doe", email: "john@doe.com" }]
   });
@@ -27,11 +27,12 @@ if (args) {
   let data = args;
   data.curve = data[1];
   data.email = data[3];
-  data.format = data[5];
-  data.name = data[7];
-  data.passphrase = data[9];
-  data.sign = data[11];
-  data.size = data[13];
-  data.type = data[15];
+  data.expiration = data[5];
+  data.format = data[7];
+  data.name = data[9];
+  data.passphrase = data[11];
+  data.sign = data[13];
+  data.size = data[15];
+  data.type = data[17];
   generateKey(data);
 }
