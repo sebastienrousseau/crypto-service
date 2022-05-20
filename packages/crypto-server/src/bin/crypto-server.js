ae2a0a39e78edc15492096780a0669273a0e9cb1
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-05-20 10:48:36 +0100

    Fix server

--- packages/crypto-server/src/bin/crypto-server.js
+++ packages/crypto-server/src/bin/crypto-server.js
@@ -7,80 +7,50 @@ import fastifyHealthcheck from "fastify-healthcheck";
 import generate from "@sebastienrousseau/crypto-core/src/lib/generate.js";
 import helmet from "@fastify/helmet";
 
-/* Taking the arguments from the command line and putting them into an array. */
+// -----------------------------------------------------------------------------
+// CLI ARGS
+// -----------------------------------------------------------------------------
+
+/* CLI arguments. */
 const args = process.argv.slice(2);
-
-const PRODUCTION = process.env.NODE_ENV === "production";
-const DEVELOPMENT = !PRODUCTION;
-const PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || "localhost";
+const PORT = parseInt(process.env.PORT, 10) || 3000;
 
+/**
+ * Create a new instance of the fastify server.
+ */
 const CryptoServer = async() => {
   /* Creating a new instance of the fastify server. */
   const app = Fastify({
-    connectionTimeout: 0,
-    keepAliveTimeout: 5000,
-    maxParamLength: 100,
     bodyLimit: 256 * 1024 * 1, // 256KB
     caseSensitive: true,
+    connectionTimeout: 0,
+    disableRequestLogging: false,
     ignoreTrailingSlash: false,
-    disableRequestLogging: PRODUCTION,
-    return503OnClosing: true,
-    logger: {
-      // level: PRODUCTION ? 'error' : 'info',
-      // level: 'info',
-      level: "error",
-    },
-    onProtoPoisoning: "error",
+    keepAliveTimeout: 5000,
+    logger: { level: "error" },
+    maxParamLength: 100,
     onConstructorPoisoning: "error",
+    onProtoPoisoning: "error",
+    return503OnClosing: true,
     trustProxy: ["127.0.0.0/8", "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"],
   });
 
   // Body content-type parsing
-  // https://www.fastify.io/docs/latest/ContentTypeParser/
   app.addContentTypeParser("*", function(request, payload, done) {
     // pipe-it directly, we don't care for it
     done();
   });
 
-  // Helmet config
-  // Defaults are commented out
-  app.register(helmet, {
-    contentSecurityPolicy: false,
-    dnsPrefetchControl: false,
-    expectCt: false,
-    // frameguard: {
-    //   action: 'sameorigin',
-    // },
-    hidePoweredBy: false,
-    hsts:
-      PRODUCTION && !argv.test ?
-        {
-          maxAge: 365 * 24 * 60 * 60,
-          includeSubDomains: false, // * no because we have non-ssl subdomains (e.g. legacy.lwjgl.org)
-          preload: false, // ! includeSubDomains must be true for preloading to be approved
-        } :
-        false,
-    ieNoOpen: false,
-    // noSniff
-    permittedCrossDomainPolicies: false,
-    referrerPolicy: {
-      policy: "no-referrer-when-downgrade",
-    },
-    // xssFilter:
-  });
 
-  await app.register(import("@fastify/compress"), {
-    global: true
-  });
   console.log(process.argv);
 
   // ---------------------------------------------------------------------------
   // ROUTES
   // ---------------------------------------------------------------------------
 
-  /* This is a route handler. It is a function that is called when a request is made
-  to the server. */
+  /* This is a route handler. It is a function that is called when a request is
+  made to the server. */
   app.get("/v1/generate", async(request, reply) => {
     console.log(request.headers);
     let generateKeyPair = await generate({ ...request.headers });
@@ -104,12 +74,6 @@ const CryptoServer = async() => {
   // ---------------------------------------------------------------------------
   // ERROR HANDLING
   // ---------------------------------------------------------------------------
-
-  // // This will never fire, since we capture all requests above
-  // app.setNotFoundHandler((request, reply) => {
-  //   reply.header('Cache-Control', 'public, max-age=30, s-maxage=0');
-  //   reply.code(404);
-  // });
 
   app.setErrorHandler((error, request, reply) => {
     // Log error
@@ -155,15 +119,47 @@ const CryptoServer = async() => {
   // PLUGINS
   // ---------------------------------------------------------------------------
 
+  /* Registering the plugins. */
   app.register(Etag);
   app.register(Accepts);
-  app.register(helmet, { global: true });
-
-  app.register(fastifyHealthcheck, {
-    healthcheckURL: "/health",
+  app.register(fastifyHealthcheck, { healthcheckURL: "/health", });
+  // Helmet config
+  app.register(helmet, {
+    contentSecurityPolicy: false,
+    dnsPrefetchControl: false,
+    expectCt: false,
+    frameguard: {
+      action: "sameorigin",
+    },
+    hidePoweredBy: false,
+    hsts:
+      {
+        maxAge: 365 * 24 * 60 * 60,
+        includeSubDomains: false,
+        preload: false, // ! includeSubDomains must be true for preloading to be approved
+      },
+    ieNoOpen: false,
+    // noSniff
+    permittedCrossDomainPolicies: false,
+    referrerPolicy: {
+      policy: "no-referrer-when-downgrade",
+    },
+    // xssFilter:
   });
 
-  /* Telling the server to listen on port 3000. */
+  /* A plugin that compresses the response. */
+  await app.register(import("@fastify/compress"), {
+    global: true
+  });
+
+  // ---------------------------------------------------------------------------
+  // LAUNCH
+  // ---------------------------------------------------------------------------
+
+  /**
+   * The function starts the server and listens on the port and host specified in
+   * the environment variables
+   */
   const start = async() => {
     try {
       await app.listen(PORT, HOST);
@@ -175,6 +171,7 @@ const CryptoServer = async() => {
   };
   start();
 };
+
 /* Calling the function with the arguments. */
 CryptoServer({
   curve: args[1],
