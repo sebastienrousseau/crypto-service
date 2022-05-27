74a8bb7282f11ed43d9216926468adb116c739f6
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-05-27 16:40:43 +0100

    Fix logger

--- packages/crypto-server/src/index.ts
+++ packages/crypto-server/src/index.ts
@@ -2,11 +2,8 @@ import Accepts from "@fastify/accepts";
 import Etag from "@fastify/etag";
 import fastify from "fastify";
 import fastifyHealthcheck from "fastify-healthcheck";
+import logger from "./lib/logger";
 import routes from './routes';
-
-// import logger from "./logger/logger";
-
-
 
 const app = fastify({
   bodyLimit: 256 * 1024 * 1, // 256KB
@@ -15,41 +12,29 @@ const app = fastify({
   disableRequestLogging: false,
   ignoreTrailingSlash: false,
   keepAliveTimeout: 5000,
-  logger: { level: "error" },
+  logger: true,
   maxParamLength: 100,
   onConstructorPoisoning: "error",
   onProtoPoisoning: "error",
   return503OnClosing: true,
-  trustProxy: ["127.0.0.0/8", "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"],
+  trustProxy: true,
 });
 
-
-
-
-// -----------------------------------------------------------------------------
-// CLI ARGS
-// -----------------------------------------------------------------------------
-
-/* CLI arguments. */
-// const args = process.argv.slice(2);
+/* Environment variables. */
 const PROTOCOL = process.env.PROTOCOL || "http";
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.NODE_PORT || 3000;
 
+/* Console output. */
 const consoleOutput = [
   "\n → protocol: " + PROTOCOL,
   "\n → hostname: " + HOST,
   "\n → port: " + PORT,
   "\n",
 ];
+logger.info("\n\nEnvironment details: " + consoleOutput);
 
-console.info("\n\nEnvironment details: " + consoleOutput);
-
-// ---------------------------------------------------------------------------
-// PLUGINS
-// ---------------------------------------------------------------------------
-
-// Register the Health plugin
+/* Register the Health plugin */
 app.register(Accepts);
 app.register(Etag);
 app.register(fastifyHealthcheck, { healthcheckUrl: "/health" });
@@ -59,22 +44,11 @@ app.register(import("@fastify/compress"), {
   global: true,
 });
 
-// ---------------------------------------------------------------------------
-// ROUTES
-// ---------------------------------------------------------------------------
-
-/* This is a route handler. It is a function that is called when a request is
-  made to the server. */
 app.get("/", async () => {
-  return { hello: "Hello Crypto Service!" };
+  return { hello: "Hello Crypto Server!" };
 });
 
 routes(app);
-
-
-// ---------------------------------------------------------------------------
-// LAUNCH
-// ---------------------------------------------------------------------------
 
 /**
  * The function starts the server and listens on the port and host specified in
@@ -83,7 +57,7 @@ routes(app);
 const start = async () => {
   try {
     await app.listen(PORT, HOST).then(() => {
-      // logger.info(`Server listening on http://${HOST}:${PORT}/`);
+      logger.info(`Server listening on http://${HOST}:${PORT}/`);
     });
   } catch (err) {
     app.log.error(err);
