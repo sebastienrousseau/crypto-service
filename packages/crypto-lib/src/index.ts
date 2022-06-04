f51bce6f01f4c4b40b1678eff16ebaac7630d8a2
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-06-04 09:54:23 +0100

    refactor: simplifying functions and exports by @sebastienrousseau

--- packages/crypto-lib/src/index.ts
+++ packages/crypto-lib/src/index.ts
@@ -1,20 +1,14 @@
-/* Importing the cryptoLib class from the crypto-lib file. */
-// import cryptoLib from "./bin/crypto-lib";
-
-// export default cryptoLib;
-
 /**
  * Export high level API functions.
  * Usage:
  *
  *   import { encrypt } from 'cryptolib';
- *   encrypt({ message, publicKeys });
+ *   encrypt({ passphrase, message });
  */
 export {
   generate, encrypt, decrypt
 } from './cryptolib';
 
-export { PublicKey, PrivateKey } from './key';
-
-
-
+export {
+  PublicKey, PrivateKey, publicKey, privateKey
+} from './key/key';
--- packages/crypto-lib/src/lib/encrypt.ts
+++ packages/crypto-lib/src/lib/encrypt.ts
@@ -1,76 +1,27 @@
 import * as openpgp from "openpgp";
-import { readFileSync } from "fs";
+import { PublicKey, PrivateKey } from "../key/key";
 
-/* Taking the arguments from the command line and storing them in an array. */
 const args = process.argv.slice(2);
-// console.log(args);
 
-/**
- * Encryption API
- * @function encrypt
- * @description Encrypts message text/data with public keys, passwords or both at once. At least either public keys or passwords must be specified. If private keys are specified, those will be used to sign the message.
- * @author Sebastien Rousseau <hello@crypto-service.co>
- * @param {Message} data.message - (required) message to be encrypted.
- * @param {String} [data.passphrase] (optional) - A passphrase to encrypt the message key.
- * @param {Key|Array<Key>} [publicKeys] - (optional) array of keys or single key, used to encrypt the message
- * @param {Key|Array<Key>} [privateKeys] - (optional) private keys for signing. If omitted message will not be signed.
- *
- *     {
- *       data: String|ReadableStream<String>|NodeStream, (if `armor` was true, the default)
- *       data.message: Message,
- *       data.passphrase: String,
- *       data.publicKey: Key|Array<Key>,
- *       data.privateKey: Key|Array<Key>,
- *     }
- * @async
- * @static
- */
 const encrypt = async (data: { passphrase: string; message: string; }) => {
   const message = data.message;
   const passphrase = data.passphrase;
-  const privateKeyArmored = await readFileSync(__dirname + "/../key/rsa.priv.pgp");
-  const publicKeyArmored = await readFileSync(__dirname + "/../key/rsa.pub.pgp");
 
-  /**
-   * Reading the public key from the file.
-   */
-  const publicKeyRead = await openpgp.readKey({
-    armoredKey: publicKeyArmored.toString(),
-  });
-
-  /**
-   * Unlock a private key with a given passphrase.
-   * @param {Object} options
-   * @param {PrivateKey} options.privateKey - The private key to decrypt.
-   * @param {String|Array<String>} options.passphrase - The user's passphrase.
-   */
   const privateKeyRead = await openpgp.decryptKey({
-    privateKey: await openpgp.readPrivateKey({
-      armoredKey: privateKeyArmored.toString(),
-    }),
+    privateKey: await PrivateKey,
     passphrase,
   });
 
-  /**
-   * Encrypts a message using a public key.
-   * If signing keys are specified, those will be used to sign the message.
-   * @param {message} message to be encrypted as created by createMessage.
-   * @param {encryptionKeys} (optional) array of keys or single key, used to encrypt the message.
-   * @param {signingKeys} (optional) private keys for signing. If omitted message will not be signed.
-   */
   const encrypted = await openpgp.encrypt({
     message: await openpgp.createMessage({ text: message }),
-    encryptionKeys: publicKeyRead,
+    encryptionKeys: await PublicKey,
     signingKeys: privateKeyRead,
   });
 
-  /* Logging the encrypted message to the console. */
   console.log(encrypted);
   return encrypted;
 };
 
-/* Checking if the arguments passed to the function are an array and if the
-array has a length. */
 if (args instanceof Array && args.length) {
   const data = {
     passphrase: args[1],
@@ -81,5 +32,4 @@ if (args instanceof Array && args.length) {
   encrypt(data);
 }
 
-/* Exporting the function `encrypt` so that it can be used in other files. */
 export default encrypt;
