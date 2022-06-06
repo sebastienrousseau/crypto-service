042510f99c3cb6c1e6d60e1cf464add66232f9d7
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-06-06 10:49:58 +0100

    test: added missing tests for generateKey

---
+++ packages/crypto-lib/test/lib/generate.test.ts
@@ -0,0 +1,106 @@
+import { generate } from "../../src/cryptolib"
+import chai from "chai";
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+const { expect, assert } = chai;
+const should = chai.should();
+
+
+const bits = 2048;
+const curve = '';
+const email = 'jane@doe.com';
+const expiration = 0;
+const format = 'armored';
+const name = 'Jane Doe';
+const passphrase = '123456789abcdef';
+const signature = true;
+const type = 'rsa';
+
+
+describe('generateKey', function () {
+  it('should generate a key', async function () {
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.fulfilled;
+  });
+
+  it('should fail for invalid bits (KeyOptions: rsaBits)', async function () {
+    const bits = 2046;
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith(/rsaBits should be at least 2047/);
+  });
+
+  it('should fail for invalid curve (KeyOptions: EllipticCurveName)', async function () {
+    const curve = 'jane@doe.com';
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith;
+  });
+
+  it('should fail for invalid user email address (KeyOptions: userIDs)', async function () {
+    const email = 'wrongemailformat.com';
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith(/Invalid user ID format/);
+  });
+
+  it('should fail for invalid expiration (KeyOptions: keyExpirationTime)', async function () {
+    const expiration = 0 - 3;
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith;
+  });
+
+  it('should fail for invalid format (KeyOptions: format)', async function () {
+    const format = 'abcdef123456789';
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith;
+  });
+
+  it('should fail for invalid user name (KeyOptions: userIDs)', async function () {
+    const name = '';
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith;
+  });
+
+  it('should fail for invalid passphrase (KeyOptions: passphrase)', async function () {
+    const passphrase = 'abcdef123456789';
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith;
+  });
+
+  it('should fail for invalid type (KeyOptions: type)', async function () {
+    const type = "123456789";
+    const test = generate({ bits, curve, email, expiration, format, name, passphrase, signature, type });
+    await expect(test).to.eventually.be.rejectedWith;
+  });
+});
+
+describe('generateKey - unit tests', function () {
+  it('should have default params set', function () {
+    const opt = {
+      bits: 2048,
+      curve: '',
+      email: 'jane@doe.com',
+      expiration: 0,
+      format: 'armored',
+      name: 'Jane Doe',
+      passphrase: '123456789abcdef',
+      signature: true,
+      type: 'rsa',
+    };
+    return generate(opt).then(async function () {
+      for (const key of [opt]) {
+        expect(key).to.exist;
+        expect(key.bits).to.equal(2048);
+        expect(key.curve).to.equal('');
+        expect(key.email).to.equal('jane@doe.com');
+        expect(key.expiration).to.equal(0);
+        expect(key.format).to.equal('armored');
+        expect(key.name).to.equal('Jane Doe');
+        expect(key.passphrase).to.equal('123456789abcdef');
+        expect(key.signature).to.equal(true);
+        expect(key.type).to.equal('rsa');
+      }
+    });
+  });
+});
+
