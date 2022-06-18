b2a14ea981d7755a9f161fb285140c75914e6830
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-06-18 15:09:51 +0100

    fix: updated unit tests by @sebastienrousseau

--- packages/crypto-lib/__tests__/lib/sign.test.ts
+++ packages/crypto-lib/__tests__/lib/sign.test.ts
@@ -1,14 +1,17 @@
-import { sign } from "../../src/bin/cryptolib"
+import { sign } from "../../src/lib/sign"
 import chai from "chai";
 import chaiAsPromised from 'chai-as-promised';
+
 chai.use(chaiAsPromised);
+
 const { expect } = chai;
 const message = 'Hello Crypto Service APIs!';
 const passphrase = '123456789abcdef';
+const detached = true;
 
 describe('sign', function () {
   it('should sign a message', async function () {
-    const test = sign({ passphrase, message });
+    const test = sign({ message, detached, passphrase });
     await expect(test).to.eventually.be.fulfilled;
   });
 });
--- packages/crypto-lib/src/lib/decrypt.ts
+++ packages/crypto-lib/src/lib/decrypt.ts
@@ -49,10 +49,11 @@ export const decrypt = async (data: types.dataDecrypt): Promise<object> => {
   const message = Buffer.from(data.encryptedMessage, "base64").toString("utf-8");
   const passphrase = data.passphrase;
 
-  const privateKeyBase64 = readFileSync(__dirname + "/../key/rsa.key");
   const publicKeyArmored = Buffer.from(data.publicKey.toString(), "base64").toString("utf-8");
-  const privateKeyArmored = Buffer.from(privateKeyBase64.toString(), "base64").toString("utf-8");
   const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
+
+  const privateKeyBase64 = readFileSync(__dirname + "/../key/rsa.key");
+  const privateKeyArmored = Buffer.from(privateKeyBase64.toString(), "base64").toString("utf-8");
   const privateKey = await openpgp.decryptKey({
     privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyArmored }),
     passphrase,
