3c94345d3f6fb93ad00161eb8daba7c65ad2b86a
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-07-19 15:48:55 +0100

    chore(server): added  rate limiting to verify by @sebastienrousseau

--- packages/crypto-server/src/routes/v1/verify.ts
+++ packages/crypto-server/src/routes/v1/verify.ts
@@ -1,8 +1,13 @@
+import {
+  rateLimitOptions,
+} from "../../config/constants";
 import * as fastify from 'fastify';
 import verify from '@sebastienrousseau/crypto-lib/dist/lib/verify';
+import fastifyRateLimit from "@fastify/rate-limit";
 import { IHeadersVerify } from '../../@types/types';
 
 export default (app: fastify.FastifyInstance) => {
+  app.register(fastifyRateLimit, rateLimitOptions); // fastify-rate-limit plugin
   app.get<{
     Headers: IHeadersVerify;
   }>("/v1/verify", async (request, reply) => {
