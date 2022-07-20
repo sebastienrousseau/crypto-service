d34a72c09a47e821d850a4d57a0568ab3fd24769
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-07-20 06:18:24 +0100

    fix(cli): refactored types by @sebastienrousseau

--- packages/crypto-cli/src/commands/verify.command.ts
+++ packages/crypto-cli/src/commands/verify.command.ts
@@ -14,32 +14,9 @@ const handleVerify = async () => {
       message: "Provide an array of publicKeys or single key, to verify signatures in base64 format",
     },
     {
-      type: "select",
-      name: "expectSigned",
-      message: "Is the message signed?",
-      choices: [
-        { title: "Yes", value: true },
-        { title: "No", value: false },
-      ],
-    },
-    {
-      type: "select",
-      name: "format",
-      message: "Select how to return the data.",
-      choices: [
-        { title: "Utf8", value: "utf8" },
-        { title: "Binary", value: "binary" },
-      ],
-    },
-    {
       type: "text",
-      name: "config",
-      message: "Provide a custom configuration settings to overwrite those in config",
-    },
-    {
-      type: "confirm",
-      name: "detached",
-      message: "Provide true or false",
+      name: "date",
+      message: "Provide an ISO Date string formatted date to verify message. If not provided, current date will be used. (YYYY-MM-DDTHH:mm:ss.sssZ)",
     },
   ]);
 
@@ -48,21 +25,13 @@ const handleVerify = async () => {
   const data = {
     message: responseVerify.message,
     verificationKeys: responseVerify.verificationKeys,
-    expectSigned: responseVerify.expectSigned,
-    format: responseVerify.format,
-    signature: responseVerify.signature,
     date: new Date(),
-    config: responseVerify.config,
   };
 
   if (
     responseVerify.message === "" ||
     responseVerify.verificationKeys === "" ||
-    responseVerify.expectSigned === "" ||
-    responseVerify.format === "" ||
-    responseVerify.signature === "" ||
-    responseVerify.date === "" ||
-    responseVerify.config === ""
+    responseVerify.date === ""
   ) {
     console.error(
       "\n🔔 You must provide a value for each of the properties.\n",
